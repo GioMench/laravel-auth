358f7712bb3501d93d22f69dd7848b51ac8f477e
GioMench <giorgiaglm@gmail.com>
2024-05-21 22:19:23 +0200

    cre4ate view show post

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('content')
+    <header class="py-3 bg-dark text-light">
+        <div class="container">
+            <h1>{{ $post->title }}</h1>
+        </div>
+
+
+    </header>
+    <section class="py-5">
+        <div class="container">
+
+            <img src=" {{ $post->cover_image }}" alt="">
+            <p>{{$post->content}}</p>
+
+        </div>
+    </section>
+@endsection
