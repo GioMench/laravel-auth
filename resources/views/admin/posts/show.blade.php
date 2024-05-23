74ae073cf33291bdc0e919c60bae2caef7927a38
GioMench <giorgiaglm@gmail.com>
2024-05-23 17:59:09 +0200

    fix structure

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -10,14 +10,24 @@
     </header>
     <section class="py-5">
         <div class="container">
+            <div class="row">
+                <div class="col">
+                    @if (Str::startsWith($post->cover_image, 'https://'))
+                        <img width="400px" src=" {{ $post->cover_image }}" alt="{{ $post->title }}">
+                    @else
+                        <img width="400px" src="{{ asset('storage/' . $post->cover_image) }}" alt="{{ $post->title }}">
+                    @endif
+                </div>
+                <div class="col">
+                    <h4 class="text-muted">description</h4>
+                
+                    <p>{{ $post->content }}</p>
+                </div>
+            </div>
 
-            @if (Str::startsWith($post->cover_image, 'https://'))
-                <img width="400px" src=" {{ $post->cover_image }}" alt="{{ $post->title }}">
-            @else
-                <img width="400px" src="{{ asset('storage/' . $post->cover_image) }}" alt="{{ $post->title }}">
-            @endif
 
-            <p>{{ $post->content }}</p>
+
+            
 
         </div>
     </section>
