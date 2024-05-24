67ef8342819b39a26859833c02def3f411ba3278
GioMench <giorgiaglm@gmail.com>
2024-05-24 12:47:13 +0200

    clean repo

--- resources/views/partials/validation-message.blade.php
+++
@@ -1,8 +0,0 @@
-@if ($errors->any())
-    <div class="alert alert-danger" role="alert">
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        <ul></ul>
-    </div>
-@endif
